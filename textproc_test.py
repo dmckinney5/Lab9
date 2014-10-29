725e121b6f4db9aa310940c30f1c4bfea99f1cac
dmckinney5 <dymc7938@colorado.edu>
2014-10-29 15:31:06 -0600

    Update textproc_test.py
    first version, done at home. maybe some shit to fix during lab

--- textproc_test.py
+++ textproc_test.py
@@ -30,6 +30,35 @@ class TextprocTestCase(unittest.TestCase):
         text = "tesing123"
         p = textproc.Processor(text)
         self.assertEqual(p.text, text, "'text' does not match input")
+        
+    def test_isString(self):
+        notText = 13579
+        self.assertRaises(textproc.TextProcError, textproc.Processor, notText)
+        
+    def test_count(self):
+        text =abcdef
+        text1 = textproc.Processor(text).count()
+        self.assertEqual(text1, 6, "Wrong Length")
+        
+    def test_count_alpha(self):
+        tigers = "tigers555"
+        tigers1 = textproc.Processor(tigers).count_alpha()
+        self.assertEqual(tigers1, 6, "Wrong Length")
+        
+    def test_count_numeric(self):
+        inverseTigers = "tigers555"
+        tigers1 = textproc.Processor(tigers1).count_numeric()
+        self.assertEqual(tigers1, 3, "Wrong Length") 
+    
+    def test_count_vowels(self):
+        alpha = "abcdefghijklmnopqrstuvwxyz"
+        alpha1 = textproc.Processor(alpha).count_vowels()
+        self.assertEqual(alpha1, 5, "Wrong Vowels")
+        
+    def test_is_phonenumber(self):
+        phone = "303-587-7030"
+        phone1 = textproc.Processor(phone).is_phonenumber()
+        self.assertTrue(phone1)
 
     # Add Your Test Cases Here...
 
